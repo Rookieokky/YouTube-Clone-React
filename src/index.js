977fd9e301f33bff7c03ae6350ef6b2daef6a100
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-07 09:18:00 -0500

    creates component files installs youtube api search adds youtube api key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const YOUTUBE_API_KEY = 'AIzaSyCUGD9uQczoM0kN3HoQaaII3Eo5x-baoqA';
+
 const App = () => {
-  return <div>Hello World!</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
