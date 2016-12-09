d7236414e8d54589657a91dace501acfc335b11e
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2016-12-09 09:17:56 -0500

    refactors App to class component, sets initial state and calls default youtube api search

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyCUGD9uQczoM0kN3HoQaaII3Eo5x-baoqA';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: YOUTUBE_API_KEY, term: 'music'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
